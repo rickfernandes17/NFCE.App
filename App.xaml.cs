65ba68980abb880013260cf73462c9a986c7432d
Henrique Fernandes <rickfernandes17@gmail.com>
2025-05-08 17:20:08 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NFCE.App
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
